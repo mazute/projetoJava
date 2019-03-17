85748883c95f2f4c58bb45851d3a3ab3eca6d5a2
mazute <leo.mazute@gmail.com>
2019-03-17 00:56:54 -0300

    Implantação classe somar

--- principal/Principal.java
+++ principal/Principal.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class Principal {
 	
 	public static void main (String[] args) {
-		int num1, num2;
+		
+		Somar s = new Somar();
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Informe o primeiro número ");
-		num1 = scan.nextInt();
+		s.num1 = scan.nextInt();
 		System.out.println("Informe o segundo número ");
-		num2 = scan.nextInt();
+		s.num2 = scan.nextInt();
 		
-		int soma = num1 + num2;
+		int soma = s.somar();
 		
+		System.out.println("Total: "+soma);
 		
 	}
 
---
+++ principal/Somar.java
@@ -0,0 +1,12 @@
+package principal;
+
+public class Somar {
+	
+	public int num1;
+	public int num2;
+	
+	public int somar() {
+		return num1+num2;
+	}
+
+}
