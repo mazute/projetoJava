14e404e7a1a90225812971d4ef49c0cebd5754ce
mazute <leo.mazute@gmail.com>
2019-03-17 00:46:35 -0300

    first commit

---
+++ principal/Principal.java
@@ -0,0 +1,20 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Principal {
+	
+	public static void main (String[] args) {
+		int num1, num2;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe o primeiro número ");
+		num1 = scan.nextInt();
+		System.out.println("Informe o segundo número ");
+		num2 = scan.nextInt();
+		
+		int soma = num1 + num2;
+		
+		
+	}
+
+}
